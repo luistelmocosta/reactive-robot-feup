62069ff523000fef4bcd4656e87c7857f2ba704b
LuisCosta <luistelmocosta@gmail.com>
2017-11-09 17:12:33 +0000

    finished

--- src/wallFollowing.cpp
+++ src/wallFollowing.cpp
@@ -1,6 +1,11 @@
-# include "../include/wallFollowing.h"
-
+#include "../include/wallFollowing.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <time.h>
 using namespace std;
+
+
 
 namespace reactive_robot_feup
 {
@@ -13,12 +18,17 @@ namespace reactive_robot_feup
 			exit(0);
 		}
 
+
+
 		laser_topic = string("/") + string(argv[1]) + string("/") + string(argv[2]);
 		speeds_topic = string("/") + string(argv[1]) + string("/cmd_vel");
 
 		subscriber = n.subscribe(laser_topic.c_str(), 1, &WallFollowing::callback, this);
 
 		cmd_vel_pub = n.advertise<geometry_msgs::Twist>(speeds_topic.c_str(), 1);
+
+
+
 	}
 
 
@@ -29,6 +39,7 @@ namespace reactive_robot_feup
 	    scan = msg;
     	geometry_msgs::Twist cmd;
     	
+    	
     	float min_distance_right_fan = FLT_MAX; //-100 to 0
     	float min_distance_left_fan = FLT_MAX; //0 to 100
     	float min_distance_front = FLT_MAX; // -15 to 15
@@ -37,6 +48,16 @@ namespace reactive_robot_feup
     	float min_distance = 0.0;
     	float robot_angle = 0.0;
     	float alpha = 0.0;
+
+    	char filename[ ] = "Results.csv";
+     	fstream appendFileToWorkWith;
+
+     	appendFileToWorkWith.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
+
+
+
+
+    	
     	
     	//read the scan from the sensors
     	for(int i = 0; i < scan.ranges.size(); i++)
@@ -93,35 +114,51 @@ namespace reactive_robot_feup
 				}
 			}
 		}
+
+
 		
 		min_distance = min(min_distance_left_fan, min_distance_right_fan);
-		alpha = 90.0 - abs(robot_angle);
+		alpha = 75.0 - abs(robot_angle);
 		
 		if(min_distance <= scan.range_max) // within range of sensors
-		{	
+		{
+
+			if (!appendFileToWorkWith ) 
+			{
+      			cout << "Cannot open file, file does not exist. Creating new file..";
+
+        		appendFileToWorkWith.open(filename,  fstream::in | fstream::out | fstream::trunc);
+        		appendFileToWorkWith <<"\n";
+        		appendFileToWorkWith.close();
+        	}
+        	else
+
+      			{    // use existing file
+         			cout<<"success "<<filename <<" found. \n";
+         			cout<<"\nAppending writing and working with existing file"<<"\n---\n";
+
+         			appendFileToWorkWith << min_distance << "," << alpha << "\n";
+         			appendFileToWorkWith.close();
+         			cout<<"\n";
+
+    
+
+
+			
 			cout << "DISTANCE TO WALL: " << min_distance << endl;
 			cout << "ALPHA: " << alpha << endl;
-	
-			cmd.linear.x = 0.4;
 			//https://www.seas.upenn.edu/sunfest/docs/papers/12-bayer.pdf
-			cmd.angular.z = (-15 * (sin(dTr(alpha)) - (min_distance - 1.46))) * cmd.linear.x;
+
+			cmd.linear.x = 0.4;
+			cmd.angular.z = (-20 * (sin(dTr(alpha)) - (min_distance - 1.46))) * cmd.linear.x;
+		}
 		}
 		else //initial movement
 		{
 			cmd.linear.x = 0.3;
-			cmd.angular.z = 1.3;
+			cmd.angular.z = 0.1;
 		}
-		
-		/*if(min_distance_front <= 1.5 && min_distance_left < scan.range_max && min_distance_right < scan.range_max) // on the pipe
-		{
-			cout << "STOPPED" << endl;
-			cout << "FRONT DISTANCE: " << min_distance_front << endl;
-			cout << "LEFT DISTANCE: " << min_distance_left << endl;
-			cout << "RIGHT DISTANCE: " << min_distance_right << endl;
-			cmd.linear.x = 0.0;
-			cmd.angular.z = 0.0;
-		}*/
-    	
+  
 		cmd_vel_pub.publish(cmd);
 	}
 
